960d4135e65037fe7f447952bfa7cc2ec406efdd
cleybson7 <132108599+cleybson7@users.noreply.github.com>
2024-11-12 19:14:48 -0300

    teste
    teste

---
+++ email/src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,53 @@
+package com.ms.email.models;
+
+import com.ms.email.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_email")
+public class EmailModel implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+    private UUID userId;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private LocalDateTime sendDateEmail;
+
+    private StatusEmail statusEmail;
+
+    public UUID getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(UUID emailId) {
+        this.emailId = emailId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+}
